e4ecce541a39834dfeac3bae34c9749760238247
Daniel Latham <daniellatham11@gmail.com>
2023-03-28 17:51:54 -0500

    Add 'Add New Expense' button and conditional form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,6 +30,7 @@ const ExpenseForm = (props) => {
 
     console.log(expenseData);
     props.onSaveExpenseData(expenseData);
+
     setTitle('');
     setAmount('');
     setDate('');
@@ -51,6 +52,7 @@ const ExpenseForm = (props) => {
       </div>
     </div>
     <div className="new-expense__actions">
+      <button type='button' onClick={props.onCancel}>Cancel</button>
       <button type="submit">Add Expense</button>
     </div>
   </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './NewExpense.css';
 
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -12,10 +14,20 @@ const NewExpense = (props) => {
     };
     console.log(expenseData);
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
 
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
+
   return <div className="new-expense">
-    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+    {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
   </div>
 };
 
